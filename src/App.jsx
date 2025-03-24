00661bf7eae507439d249051040df88d5eb99407
Paula Fenner <fenner.paula@gmail.com>
2025-03-24 18:47:22 +0100

    add results table
    Added the structure of the table that will render the results.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import UserInput from './components/UserInput';
+import ResultsTable from './components/ResultsTable';
 
 export default function App() {
   return (
     <>
       <Header />;
       <UserInput />
+      <ResultsTable/>
     </>
   );
 }
---
+++ src/components/ResultsTable.jsx
@@ -0,0 +1,25 @@
+export default function ResultsTable() {
+  return (
+    <table id="result" >
+      <thead className="center">
+        <tr>
+          <th scope="col">Year</th>
+          <th scope="col">Investment Value</th>
+          <th scope="col">Interest (Year)</th>
+          <th scope="col">Total Interest</th>
+          <th scope="col">Interest Capital</th>
+        </tr>
+      </thead>
+
+      <tbody className="center">
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
